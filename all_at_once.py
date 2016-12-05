f6a765c94de08851777d403a4ec2513235fea109
Young-Chan Park <yc091893@gmail.com>
2016-12-05 13:46:17 +0000

    Made the Summary_info(human) file to write the list of transcripts that have not been grouped with any other transcripts

--- all_at_once.py
+++ all_at_once.py
@@ -64,68 +64,76 @@ def main(args):
             # Get the chunk coordinates for each transcripts
             elif (local_alignment[0] != local_alignment[1]) and (float(local_alignment[2]) >= args.i) and (local_alignment[3] >= 50):
                 matching_exons = list()
-                tscript_exon_coord = transcript_infos[local_alignment[0]]
 
-                for exons in transcript_infos[local_alignment[0]]:
-                    intron_match = (int(local_alignment[7]) - int(tscript_exon_coord[exons][1])) + (int(tscript_exon_coord[exons][0]) - int(local_alignment[6]))
-                    if (int(local_alignment[6]) > int(tscript_exon_coord[exons][0])) and (int(local_alignment[7]) < int(tscript_exon_coord[exons][1])):
-                        continue
-                        # Ignore this alignment because its shorter than the exon. Useless
-                    elif (int(local_alignment[6]) <= int(tscript_exon_coord[exons][0])) and (int(local_alignment[7]) >= int(tscript_exon_coord[exons][1])):
-                        # Matching exon and possibly some introns
-                        #print (local_alignment[6] + "  " + local_alignment[7] + "\n" + transcript_infos[local_alignment[0]][exons][0] + "  " + transcript_infos[local_alignment[0]][exons][1])
-                        # The above is just to compare between exon coordinates and alignment coordinate
-                        matching_exons.append(str(exons))
-                        print ("Exon" + str(exons) + " has " + str(intron_match) + " extra intronic matches")
-                if (len(matching_exons) == 0):
-                    print "no matching exons"
-                elif (len(matching_exons) >= 1):
-                    print ("Transcript " + local_alignment[1] + " matches " + str(matching_exons) + " of " + local_alignment[0] + "'s exon(s)")
-                    q_transc = list()
-                    s_transc = list()
-
-                    for group_num in groups :
-                        # Checks if the query and subject transcript is already grouped
-                        if (local_alignment[0] in group_dictionary[group_num]):
-                            q_transc.append(group_num)
-                                #print (local_alignment[0] + " is in Group " + str(group_num))
-                        if (local_alignment[1] in group_dictionary[group_num]):
-                            s_transc.append(group_num)
-                                #print (local_alignment[1] + " is in Group " + str(group_num))
-
-
-                    if (len(q_transc) == 0) and (len(s_transc) == 0):
-                        # Group the two transcripts into a new group if either one are not already grouped
-                        print ("Both transcripts are not within any group")
-                            # Do they have good intronic matches?
-                        if ((len(matching_exons) == 1) and (intron_match >= 30)) or (len(matching_exons) > 1):
-                            group_dictionary[group_counter] = dict()
-                            group_dictionary[group_counter][local_alignment[0]] = dict()
-                            group_dictionary[group_counter][local_alignment[1]] = dict()
-                            groups.append(group_counter)
-                            print (local_alignment[0] + " and " + local_alignment[1] + " grouped to Group" + str(group_counter))
-                            group_counter = group_counter + 1
-                            #is_there_any_match = True
-                        elif (len(matching_exons) == 0):
-                            print "Not grouped due to no matching exons or insufficient intron matches"
+                # WHY IS THE local_alignment[0] NOT IN transcript_infos??? Because the original transcript file did not contain the transcript data, OR genome walked file did not contain the genome_walked data
+                if (local_alignment[0] not in transcript_infos):
+                    print (str(local_alignment[0]) + " is not in transcript_infos.")
+                    continue
+                else :
+                    tscript_exon_coord = transcript_infos[local_alignment[0]]
 
 
 
-                                    # You need to solve the issue where one transcript is already added to a group so whether to add the non-grouped transcript to the original group or to make a new group
-                                    # I've made it so that if transcript A is grouped while transcript B isn't, then transcript B is grouped into all the groups that has transcript A
-                                    # Two problems here. 1) Order of the blast input file, 2)
-                    elif (len(q_transc) == 0) and (len(s_transc) != 0):
-                        print (local_alignment[1] + ' is in Group(s) ' + str(s_transc))
-                        print ('Adding ' + local_alignment[0] + ' into group(s)' + str(s_transc))
-                        for num in s_transc :
-                            group_dictionary[num][local_alignment[0]] = dict()
+                    for exons in transcript_infos[local_alignment[0]]:
+                        intron_match = (int(local_alignment[7]) - int(tscript_exon_coord[exons][1])) + (int(tscript_exon_coord[exons][0]) - int(local_alignment[6]))
+                        if (int(local_alignment[6]) > int(tscript_exon_coord[exons][0])) and (int(local_alignment[7]) < int(tscript_exon_coord[exons][1])):
+                            continue
+                            # Ignore this alignment because its shorter than the exon. Useless
+                        elif (int(local_alignment[6]) <= int(tscript_exon_coord[exons][0])) and (int(local_alignment[7]) >= int(tscript_exon_coord[exons][1])):
+                            # Matching exon and possibly some introns
+                            #print (local_alignment[6] + "  " + local_alignment[7] + "\n" + transcript_infos[local_alignment[0]][exons][0] + "  " + transcript_infos[local_alignment[0]][exons][1])
+                            # The above is just to compare between exon coordinates and alignment coordinate
+                            matching_exons.append(str(exons))
+                            print ("Exon" + str(exons) + " has " + str(intron_match) + " extra intronic matches")
+                    if (len(matching_exons) == 0):
+                        print "no matching exons"
+                    elif (len(matching_exons) >= 1):
+                        print ("Transcript " + local_alignment[1] + " matches " + str(matching_exons) + " of " + local_alignment[0] + "'s exon(s)")
+                        q_transc = list()
+                        s_transc = list()
+
+                        for group_num in groups :
+                            # Checks if the query and subject transcript is already grouped
+                            if (local_alignment[0] in group_dictionary[group_num]):
+                                q_transc.append(group_num)
+                                    #print (local_alignment[0] + " is in Group " + str(group_num))
+                            if (local_alignment[1] in group_dictionary[group_num]):
+                                s_transc.append(group_num)
+                                    #print (local_alignment[1] + " is in Group " + str(group_num))
 
 
-                    elif (len(q_transc) != 0) and (len(s_transc) == 0):
-                        print (local_alignment[0] + ' is in Group(s) ' + str(q_transc))
-                        print ('Adding ' + local_alignment[1] + ' into group(s)' + str(q_transc))
-                        for num in q_transc :
-                            group_dictionary[num][local_alignment[1]] = dict()
+                        if (len(q_transc) == 0) and (len(s_transc) == 0):
+                            # Group the two transcripts into a new group if either one are not already grouped
+                            print ("Both transcripts are not within any group")
+                                # Do they have good intronic matches?
+                            if ((len(matching_exons) == 1) and (intron_match >= 30)) or (len(matching_exons) > 1):
+                                group_dictionary[group_counter] = dict()
+                                group_dictionary[group_counter][local_alignment[0]] = dict()
+                                group_dictionary[group_counter][local_alignment[1]] = dict()
+                                groups.append(group_counter)
+                                print (local_alignment[0] + " and " + local_alignment[1] + " grouped to Group" + str(group_counter))
+                                group_counter = group_counter + 1
+                                #is_there_any_match = True
+                            elif (len(matching_exons) == 0):
+                                print "Not grouped due to no matching exons or insufficient intron matches"
+
+
+
+                                        # You need to solve the issue where one transcript is already added to a group so whether to add the non-grouped transcript to the original group or to make a new group
+                                        # I've made it so that if transcript A is grouped while transcript B isn't, then transcript B is grouped into all the groups that has transcript A
+                                        # Two problems here. 1) Order of the blast input file, 2)
+                        elif (len(q_transc) == 0) and (len(s_transc) != 0):
+                            print (local_alignment[1] + ' is in Group(s) ' + str(s_transc))
+                            print ('Adding ' + local_alignment[0] + ' into group(s)' + str(s_transc))
+                            for num in s_transc :
+                                group_dictionary[num][local_alignment[0]] = dict()
+
+
+                        elif (len(q_transc) != 0) and (len(s_transc) == 0):
+                            print (local_alignment[0] + ' is in Group(s) ' + str(q_transc))
+                            print ('Adding ' + local_alignment[1] + ' into group(s)' + str(q_transc))
+                            for num in q_transc :
+                                group_dictionary[num][local_alignment[1]] = dict()
 
 
 
@@ -138,7 +146,7 @@ def main(args):
     elif (len(group_dictionary) >= 1):
         print group_dictionary
         print groups
-        print "\n \n \n \n Starting gene model construction \n \n \n \n \n \n \n \n \n \n "
+        print "\n \n \n \n Starting gene model construction \n \n \n \n"
 
 
         for transcripts in hundred_percent_coordinates:
@@ -779,8 +787,7 @@ def main(args):
                             ins.write(chunks)
                         else:
                             ins.write(chunks + ', ')
-                    ins.write('\n')
-                    ins.write('\n \n \n')
+                    ins.write('\n \n \n \n')
 
 
                 # Write computer readable summary file
@@ -797,6 +804,8 @@ def main(args):
                         subprocess.call("mkdir gene_models/gene_model" + str(Gene) + "_folder", shell=True)
                     if os.path.exists("gene_models/gene_model" + str(Gene) + "_folder/final_all_seq.fasta.txt"):
                         subprocess.call('rm gene_models/gene_model' + str(Gene) + '_folder/final_all_seq.fasta.txt', shell=True)
+                    if os.path.exists("gene_models/gene_model" + str(Gene) + "_folder/all_in_one_seq.fasta.txt"):
+                        subprocess.call('rm gene_models/gene_model' + str(Gene) + '_folder/all_in_one_seq.fasta.txt', shell=True)
 
                     with open('gene_models/gene_model' + str(Gene) + '_folder/final_all_seq.fasta.txt', 'w') as ins:
                         ins.write("")
@@ -805,15 +814,40 @@ def main(args):
                             ins.write(inserting_separate_seq[idx] + "\n \n")
                         ins.close()
 
+                    with open('gene_models/gene_model' + str(Gene) + '_folder/all_in_one.fasta.txt', 'w') as ins:
+                        ins.write(">gene_model" + str(Gene) + "\n")
+                        ins.write("".join(inserting_seq)+"\n \n")
+                        ins.close()
+
+
+
 
                 # RESET FOR THE NEXT GENE MODEL!
                 inserting_seq = list()
                 inserting_separate_seq = list()
 
+
+            with open('gene_models/Summary_info_human.txt', 'a') as ins:
+                ins.write("-- Only variant transcripts -- \n")
+                for transcript in sorted(hundred_percent_coordinates, key=str):
+                    grouped = False
+                    for Gene in group_dictionary:
+                        if (transcript in group_dictionary[Gene]):
+                            grouped = True
+                            break
+                    if (grouped == False):
+                        if (sorted(hundred_percent_coordinates, key=str).index(transcript) + 1 == len(hundred_percent_coordinates)):
+                            ins.write(transcript + '\n\n\n\n\n')
+                        else:
+                            ins.write(transcript + ', ')
+
+
+
             # Remove all temporary files that are no longer needed
-            #subprocess.call("rm gene_models/tmp_blast.txt", shell=True)
-            #subprocess.call("rm gene_models/exon_coordinates.txt", shell=True)
+            subprocess.call("rm gene_models/exon_coordinates.txt", shell=True)
             subprocess.call("rm gene_models/tmp_mafft.fasta.txt", shell=True)
+            if (args.blast == False):
+                subprocess.call("rm gene_models/tmp_blast.txt", shell=True)
 
 
 
@@ -822,6 +856,7 @@ parser.add_argument('Genome_Walked_seq', metavar = 'filename.FASTA', help='Genom
 parser.add_argument('Original_seq', metavar = 'filename.FASTA', help='Original sequence file before running Genome Walker')
 parser.add_argument('-i', nargs='?', metavar='FLOAT', default=95.000, type=float, help='Minimum percentage identity for the grouping to occur based on the BLAST result (default: %(default)s)')
 parser.add_argument('-s','--separate', action='store_true', help='Creates a new folder that contains information about individual gene models (default: %(default)s)')
+parser.add_argument('-b','--blast', action='store_true', help='Does not delete the BLAST output (default: %(default)s)')
 args = parser.parse_args()
 
 main(args)
