abc21591bcfeb60e6cf6df0e97148c42ac884bde
Young-Chan Park <yc091893@gmail.com>
2017-03-07 16:27:13 +0000

    Changed mapping.py

--- mapping.py
+++ mapping.py
@@ -3,13 +3,14 @@ import subprocess
 
 
 def main(args):
-    subprocess.call("bwa mem ~/Desktop/project/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna " + args.map_seq + " > " + args.map_seq + ".sam", shell=True)
+    subprocess.call("bwa mem " + args.reference_seq + " " + args.map_seq + " > " + args.map_seq + ".sam", shell=True)
     subprocess.call("samtools sort " + args.map_seq + ".sam > " + args.map_seq + ".bam", shell=True)
     subprocess.call("samtools index " + args.map_seq + ".bam", shell=True)
     print "Done"
 
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('map_seq', metavar = 'filename.FASTA', help='Sequence file to be mapped')
+parser.add_argument('reference_seq', metavar = 'filename.FASTA', help='Reference sequence file to be mapped onto')
+parser.add_argument('map_seq', metavar = 'filename.FASTA', help='Sequence file to be mapped on')
 args = parser.parse_args()
 main(args)
